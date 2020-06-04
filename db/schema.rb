4e85c271271a3093a78e5ae6d2a96299ee876158
kazukifukuzawa <fukuzawakazuki@gmail.com>
2020-06-04 20:37:54 +0900

    articles_delate

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,4 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_095254) do
 
-  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-    t.text "text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-end
