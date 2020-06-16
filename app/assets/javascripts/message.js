c554f4b22c0474fb3938fda29da1b44df49c9e8e
kazukifukuzawa <fukuzawakazuki@gmail.com>
2020-06-16 16:35:27 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -44,7 +44,6 @@ $(function(){
     e.preventDefault();
     let formData = new FormData(this);
     let url = $(this).attr('action');
-    console.log(this);
     $.ajax({
       url: url,
       type: "POST",
