952dad5886623327e9c5825c98827b2a7af56bbf
kazukifukuzawa <fukuzawakazuki@gmail.com>
2020-06-19 15:08:28 +0900

    change2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,10 @@
 # config valid for current version and patch releases of Capistrano
 
-lock "~> 3.14.1"
+lock "3.14.1"
 
 set :application, "chatspace-git"
-set :repo_url, 'git@github.com:kazukifukuzawa/catspace-git.git'
+
+set :repo_url, 'git@github.com:kazukifukuzawa/chatspace-git.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
